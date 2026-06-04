066cc3910aff2905ee43a76b518b849f25485c64
KanjaDev3 <ryan.kanja@strathmore.edu>
2026-06-04 11:55:41 +0300

    Lab 2.7 JS 101

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let age = 20;
+var course = BBIT;
+const university = "Strathmore University";
+
+//Redeclare 
+//let age = 3;
+var course = ICS;
+//const university = "Riara University";
+
+let age = 30;
+var course = isFinite;
